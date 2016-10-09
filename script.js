99f5471afbd06532a58a033515d18534753ee773
legerg05 <legerg05@hotmail.com>
2016-10-08 18:16:15 -0600

    Nothing much

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+var myKey = vnTQx5zkeFBoM3YF;
+var myurl = "http://api.shoutcast.com/legacy/genresearch?k=" + myKey + "&genre=classic&limit=X,Y"
 
+$.ajax({
+ 		   url : myurl,
+ 		   dataType : "json",
+ 		   success : function(parsed_json) {
+		     
+ 		   }
+ 		 });
