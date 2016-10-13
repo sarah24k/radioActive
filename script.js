2f0b28a7ffad908954e4324612600ec68baf7360
Travis Fantina <tfantina@gmail.com>
2016-10-12 18:54:47 -0600

    Angular Templates, not working

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+angular.module('radioActive', ['ui.router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('music', {
+      url: '/music',
+      templateUrl: '/music.html',
+      controller: 'MusicCtrl'
+    });
+    $urlRouterProvider.otherwise('home');
+  }])
 
+  .controller('MainCtrl', [
+    '$scope', function($scope) {
+      $scope.contents = "This is content! woo";
+    }])
+
+  .controller('MusicCtrl', [
+    '$scope', '$stateParams', function($scope, $stateParams) {
+      $scope.music = "This is the music! woo";
+    }
+  ]);
