0a4159d664125f9d64f0c91c5da031a3842349f2
legerg05 <legerg05@hotmail.com>
2016-10-12 20:47:44 -0600

    Here

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-var myKey = vnTQx5zkeFBoM3YF;
-var myurl = "http://api.shoutcast.com/legacy/genresearch?k=" + myKey + "&genre=classic&limit=X,Y"
+var myKey = "vnTQx5zkeFBoM3YF";
+var myurl;
 
-$.ajax({
- 		   url : myurl,
- 		   dataType : "json",
- 		   success : function(parsed_json) {
-		     
- 		   }
- 		 });
+//Now Playing Info API
+    myurl = "http://api.shoutcast.com/station/nowplaying?k=" + myKey + "&ct=rihanna&limit=20&f=json";
+    $.ajax({
+         url : myurl,
+         dataType : "json",
+         success : function(parsed_json) {
+           console.log(parsed_json);
+         }
+    });
+
+//Genre Info API
+//    myurl = "http://api.shoutcast.com/legacy/genresearch?k=" + myKey + "&genre=classic&limit=20";
+//   $.ajax({
+//        url : myurl,
+//        dataType : "json",
+//        success : function(parsed_json) {
+//          console.log(parsed_json);
+//        }
+//    })
